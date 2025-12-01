fc625467a42c0731d89d5c4c6c2bd5b28cd1bf52
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-12-01 15:26:13 +0100

    Icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Github, Twitter, Linkedin } from 'lucide-react';
+import { Linkedin, Facebook, Instagram} from 'lucide-react';
+
 import SectionSeparator from './SectionSeparator';
 
 // Fix: Defined a dedicated interface for SocialIcon props for better type checking.
@@ -26,11 +27,18 @@ const Footer = () => {
             <p className="text-sm mt-1">Napędzamy wzrost poprzez innowacje.</p>
           </div>
           <div className="flex space-x-6">
-            <SocialIcon href="#"><Twitter size={20} /></SocialIcon>
-            <SocialIcon href="#"><Github size={20} /></SocialIcon>
-            <SocialIcon href="#"><Linkedin size={20} /></SocialIcon>
+          <SocialIcon href="https://www.facebook.com/ESENCJA"><Facebook size={20} /></SocialIcon>
+          <SocialIcon href="https://www.instagram.com/esencjareklamy"><Instagram size={20} /></SocialIcon>
+          <SocialIcon href="https://www.linkedin.com/company/esencja-kreatorzy-reklamy"><Linkedin size={20} /></SocialIcon>
+            <SocialIcon href="https://x.com/esencjaagencja">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24h-6.514l-5.106-6.694-5.829 6.694h-3.306l7.733-8.835L2.25 2.25h6.675l4.872 6.237 5.517-6.237zM17.15 18.558h1.828L6.122 3.975H4.23l12.92 14.583z"/>
+              </svg>
+            </SocialIcon>
+
           </div>
-        </div>
+
+      </div>
         <div className="mt-8 pt-8 border-t border-gray-200 dark:border-neutral-800 text-center text-sm text-gray-500 dark:text-neutral-400">
           <p>&copy; {new Date().getFullYear()} Esencja. Wszelkie prawa zastrzeżone. Autor Stanisław Korycki</p>
           <div className="mt-2">
