7bbb4801d2b45ca8811daae43e92a121c522f512
stasi0xx <stanislaw.korycki.w@gmail.com>
2025-12-16 07:50:20 +0100

    otwieranie podstron za pomocą linku

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'./',
+  base:'/',
   define: {
     'process.env.VITE_GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
   }
